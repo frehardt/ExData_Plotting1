b5928e7a365f4f84f8f9f7f6aed5cfede4e10398
frehardt <flo77@gmx.ch>
2015-02-09 09:35:03 +0100

    Change

--- plot4.R
+++ plot4.R
@@ -1,4 +1,21 @@
-source("load_data.R")
+filename <- "household_power_consumption.txt"
+data <- read.table(filename,
+                   header = TRUE,
+                   sep = ";",
+                   colClasses = c("character", "character", rep("numeric",7)),
+                   na = "?")
+
+attach(data)
+
+subset <- Date == "1/2/2007" | Date == "2/2/2007"
+newData <- data[subset, ]
+attach(newData)
+x <- paste(Date, Time)
+newData$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+rownames(newData) <- 1:nrow(newData)
+dim(newData) # 2880   10
+attach(newData)
+
 png(filename = "plot4.png", 
     width = 480, height = 480,
     units = "px", bg = "transparent")
